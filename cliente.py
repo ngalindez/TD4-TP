c91f0971174f033ba2e33a1a73b3ee5a41cb9564
ngalindez <nicogalindez@gmail.com>
2024-10-10 17:19:14 -0300

    Arreglé filter

--- cliente.py
+++ cliente.py
@@ -1,14 +1,15 @@
 import canalruidoso as f  # Correr pip install canalruidoso en la terminal
 from scapy.all import *  # Correr pip install scapy en la terminal
+import time
 
 # Elegimos parametros
-source_ip = '127.0.0.1'
+src_ip = '127.0.0.1'
 dest_ip = '127.0.0.1'
-dest_port = 8000
 src_port = 5000
+dest_port = 8000
 
 # Creamos la parte de IP
-ip = IP(dst=dest_ip, src=source_ip)
+ip = IP(dst=dest_ip, src=src_ip)
 
 # Creamos la parte de TCP
 tcp = TCP(dport=dest_port, sport=src_port, flags="S")
@@ -18,8 +19,11 @@ tcp = TCP(dport=dest_port, sport=src_port, flags="S")
 packet = ip/tcp
 
 
-# "Enviamos" el paquete
-f.envio_paquetes_inseguro(packet)
+def envio_paquetes_seguro(seq, ack, flags, src_ip, dest_ip, src_port, dest_port):
+    ip = IP(dst=dest_ip, src=src_ip)
+    tcp = TCP(dport=dest_port, sport=src_port, flags=flags, seq=seq, ack=ack)
+    packet = ip/tcp
+    f.envio_paquetes_inseguro(packet)
 
 
-def envio_paquetes_seguro(packet, )
+envio_paquetes_seguro(11, 0, "S", '127.0.0.1', '127.0.0.1', 5000, 8000)
--- servidor.py
+++ servidor.py
@@ -1,4 +1,5 @@
 from scapy.all import *
+from scapy.all import TCP
 import canalruidoso as f  # Correr pip install canalruidoso en la terminal
 
 # Mostramos todas las interfaces
@@ -11,17 +12,22 @@ def info_packet(packet):
     print(f"Flags: {packet[TCP].flags}")
     print(f"#SEQ: {packet[TCP].seq}")
     print(f"#ACK: {packet[TCP].ack}")
-    print(f"Checksum {packet[TCP].chksum}\n")
+    print(f"Checksum: {packet[TCP].chksum}\n")
+
+
+def filter_function_server(packet):
+    return packet.haslayer(TCP) and packet[TCP].dport == 8000
 
 
 # Esto lo tienen que completar con el nombre de la interfaz que tenga el 127.0.0.1 si se recibe el paquete en la misma computadora que lo envio.
 interface = "lo0"
 
-listen_port = 8000  # Elegir el puerto que esta escuchando
 
-print(f"Listening for TCP packets on port {listen_port}...")
-filter_str = f"tcp port {listen_port}"
+def listen(listen_port):
+    print(f"Listening for TCP packets on port {listen_port}...")
+    pkt_capturado = sniff(
+        iface=interface, prn=info_packet, count=1, timeout=60, lfilter=filter_function_server)
+    return pkt_capturado
 
-# Escuchar en ese puerto
-pkt_capturado = sniff(
-    iface=interface, prn=info_packet, count=1, timeout=60)
+
+listen(8000)
