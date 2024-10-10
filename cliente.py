b3432acddafabaa12fd77a7bb99e02e1d6954097
ngalindez <nicogalindez@gmail.com>
2024-10-10 17:59:00 -0300

    agregué funciones

--- cliente.py
+++ cliente.py
@@ -1,6 +1,6 @@
 import canalruidoso as f  # Correr pip install canalruidoso en la terminal
 from scapy.all import *  # Correr pip install scapy en la terminal
-import time
+from funciones import *
 
 # Elegimos parametros
 src_ip = '127.0.0.1'
@@ -8,22 +8,6 @@ dest_ip = '127.0.0.1'
 src_port = 6000
 dest_port = 9000
 
-# Creamos la parte de IP
-ip = IP(dst=dest_ip, src=src_ip)
-
-# Creamos la parte de TCP
-tcp = TCP(dport=dest_port, sport=src_port, flags="S")
-
-
-# Los combinamos
-packet = ip/tcp
-
-
-def envio_paquetes_seguro(seq, ack, flags, src_ip, dest_ip, src_port, dest_port):
-    ip = IP(dst=dest_ip, src=src_ip)
-    tcp = TCP(dport=dest_port, sport=src_port, flags=flags, seq=seq, ack=ack)
-    packet = ip/tcp
-    f.envio_paquetes_inseguro(packet)
-
 
 envio_paquetes_seguro(11, 0, "S", '127.0.0.1', '127.0.0.1', 6000, 9000)
+pkt = listen(6000)
--- servidor.py
+++ servidor.py
@@ -1,33 +1,14 @@
 from scapy.all import *
 from scapy.all import TCP
 import canalruidoso as f  # Correr pip install canalruidoso en la terminal
+from funciones import *
 
-# Mostramos todas las interfaces
-# print(conf.ifaces)
+# Parámetros
+src_ip = '127.0.0.1'
+dest_ip = '127.0.0.1'
+src_port = 9000
+dest_port = 6000
 
-
-def info_packet(packet):
-    print(f"\nSource port: {packet[TCP].sport}")
-    print(f"Destination port: {packet[TCP].dport}")
-    print(f"Flags: {packet[TCP].flags}")
-    print(f"#SEQ: {packet[TCP].seq}")
-    print(f"#ACK: {packet[TCP].ack}")
-    print(f"Checksum: {packet[TCP].chksum}\n")
-
-
-def filter_function_server(packet):
-    return packet.haslayer(TCP) and packet[TCP].dport == 8000
-
-
-# Esto lo tienen que completar con el nombre de la interfaz que tenga el 127.0.0.1 si se recibe el paquete en la misma computadora que lo envio.
-interface = "lo0"
-
-
-def listen(listen_port):
-    print(f"Listening for TCP packets on port {listen_port}...")
-    pkt_capturado = sniff(
-        iface=interface, filter='tcp port 9000', prn=info_packet, count=1, timeout=60)
-    return pkt_capturado
-
-
-listen(9000)
+pkt = listen(9000)
+envio_paquetes_seguro(500, pkt[TCP].seq + 1, "SA",
+                      src_ip, dest_ip, src_port, dest_port)
