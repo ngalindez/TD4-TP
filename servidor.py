1706fd4bc6b4649da99ae59fde88c0f65feda523
NicoPalazon <nicolaspalazon01@gmail.com>
2024-10-15 15:42:42 -0300

    a

--- servidor.py
+++ servidor.py
@@ -33,3 +33,19 @@ time.sleep(timer_)
 #    f.envio_paquetes_inseguro(enviar_pkt(tcp_pkt.ack, tcp_pkt.seq, "F"))
 
 # print(pkt_capturado.stats)
+
+
+_src_ip = '127.0.0.1'
+_dest_ip = '127.0.0.1'
+_src_port = 9000
+_dest_port = 6000
+_interface = 'lo0'
+
+servidor = SocketRDT(_src_ip, _src_port, _dest_ip, _dest_port, _interface)
+
+# pkt = servidor.listen()
+while servidor.last_pkt_rcvd == None:
+    pkt = servidor.listen()
+# servidor.envio_paquetes_seguro('A')
+# servidor.listen()
+servidor.terminar_conexion()
