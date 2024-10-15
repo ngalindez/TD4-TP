7ace976a58260c21e58cf52c33dd7ab9ae70e802
NicoPalazon <nicolaspalazon01@gmail.com>
2024-10-15 18:39:55 -0300

    a

--- cliente.py
+++ cliente.py
@@ -37,25 +37,3 @@ while not tcp_pkt or (TCP in tcp_pkt[0] and tcp_pkt[0][TCP].ack < seq_ + 1):
     # Inicia el hilo de envío de paquetes
     enviar_thread.start()
     tcp_pkt = escuchar(3, src_port)
-
-
-import canalruidoso as f  # Correr pip install canalruidoso en la terminal
-from scapy.all import *  # Correr pip install scapy en la terminal
-from funciones import *
-
-# Parametros
-_src_ip = '127.0.0.1'
-_dest_ip = '127.0.0.1'
-_src_port = 6000
-_dest_port = 9000
-_interface = 'lo0'
-
-cliente = SocketRDT(_src_ip, _src_port, _dest_ip, _dest_port, _interface)
-cliente.iniciar_conexion()
-print(cliente.conn_established)
-while cliente.conn_established:
-    cliente.listen()
-    if not cliente.conn_established:
-        break
-    cliente.envio_paquetes_seguro('A')
-
--- funciones.py
+++ funciones.py
@@ -1,106 +1,36 @@
-from scapy.all import *
 from scapy.all import TCP, IP
-import canalruidoso as f
+from scapy.all import *
+import canalruidoso as f  # Correr pip install canalruidoso en la terminal
 import time
 
+def info_packet(packet):
+    
 
-# Elegimos #SEQ aleatorio para el primer paquete
-rand = random.randint(0, 1024)
+    print(f"\nSource port: {packet[0][TCP].sport}")
+    print(f"Destination port: {packet[0][TCP].dport}")
+    print(f"Flags: {packet[0][TCP].flags}")
+    print(f"#SEQ: {packet[0][TCP].seq}")
+    print(f"#ACK: {packet[0][TCP].ack}")
+    print(f"Checksum {packet[0][TCP].chksum}\n")
 
-char2flag = {'S': 'SYN', 'A': 'ACK', 'F': 'FIN',
-             'SA': 'SYN + ACK', 'FA': 'FIN + ACK', 'RST': 'RESET'}
 
+def enviar_pkt(seq_a, ack_a, flags_, dest_ip, source_ip, dest_port, src_port):
+    ip = IP(dst=dest_ip, src=source_ip)
 
-class SocketRDT:
+    # Creamos la parte de TCP
+    tcp = TCP(dport=dest_port, sport=src_port, flags = flags_, seq = seq_a, ack = ack_a + 1)
 
-    def info_packet(self, packet):
-        print(f"\nSource port: {packet[TCP].sport}")
-        print(f"Destination port: {packet[TCP].dport}")
-        print(f"Flags: {packet[TCP].flags}")
-        print(f"#SEQ: {packet[TCP].seq}")
-        print(f"#ACK: {packet[TCP].ack}")
-        print(f"Checksum: {packet[TCP].chksum}\n")
+    # Los combinamos
+    packet = ip/tcp
+    info_packet(packet)
+    return packet
 
-    def __init__(self, src_ip, src_port, dest_ip, dest_port, interface):
-        self.src_ip = src_ip
-        self.src_port = src_port
-        self.dest_ip = dest_ip
-        self.dest_port = dest_port
-        self.interface = interface
-        self.last_pkt_rcvd = None
-        self.last_pkt_sent = None
-        self.conn_established = False
+def escuchar(timeout_, puerto_):
+    interface = "Software Loopback Interface 1"
 
-    def filter_function(self, packet):
-        return packet.haslayer(TCP) and packet[TCP].dport == self.src_port
+    print(f"Listening for TCP packets on port {puerto_}...")
+    filter_str = f"tcp port {puerto_}"
 
-    def listen(self):
-        print(f"Listening for TCP packets on port {self.src_port}...")
-        pkt_capturado = sniff(
-            iface=self.interface,
-            prn=self.info_packet,
-            count=1,
-            timeout=60,
-            lfilter=self.filter_function)
-        self.last_pkt_rcvd = pkt_capturado[0]
-
-        # Para asegurarme de que recibí el que quiero
-        # si self.last_pkt_sent == None, self.last_pkt_rcvd es el primero que recibo
-        SEQ_esperado = self.last_pkt_rcvd[TCP].ack if self.last_pkt_sent is None else self.last_pkt_sent[TCP].seq + 1
-        if self.last_pkt_rcvd[TCP].ack != SEQ_esperado:
-            self.listen()
-
-        # Para el three-way-handshake
-        if self.last_pkt_rcvd[TCP].flags == 'S' and not self.conn_established:
-
-            self.envio_paquetes_seguro(_flags='SA')
-            self.listen()
-
-            if self.last_pkt_rcvd[TCP].flags == 'A':
-                self.conn_established = True
-
-        # Para el cierre de conexión
-        if self.last_pkt_rcvd[TCP].flags == 'F' and self.conn_established:
-            self.envio_paquetes_seguro(_flags='FA')
-            self.listen()
-
-            if self.last_pkt_rcvd[TCP].flags == 'A':
-                self.conn_established = False
-
-        return self.last_pkt_rcvd
-
-    def envio_paquetes_seguro(self, _flags, last_pkt=None):
-        # Armo el paquete con las partes IP y TCP
-        last_pkt = self.last_pkt_rcvd if last_pkt is None else last_pkt
-        ip = IP(dst=self.dest_ip, src=self.src_ip)
-        tcp = TCP(dport=self.dest_port,
-                  sport=self.src_port,
-                  flags=_flags,
-                  seq=last_pkt[TCP].ack,
-                  ack=last_pkt[TCP].seq + 1)
-        packet = ip/tcp
-
-        f.envio_paquetes_inseguro(packet)
-        self.last_pkt_sent = packet
-
-    def iniciar_conexion(self):
-        # Elegimos #SEQ aleatorio para el primer paquete, lo guardo como atributo para otro caso
-        self.rand_SEQ = random.randint(0, 1024)
-        self.envio_paquetes_seguro(
-            _flags='S',
-            last_pkt=IP()/TCP(seq=-1, ack=self.rand_SEQ))
-
-        while self.last_pkt_rcvd == None or (self.last_pkt_rcvd[TCP].flags != 'SA'):
-            self.listen()
-
-        self.envio_paquetes_seguro(_flags='A')
-        self.conn_established = True
-
-    def terminar_conexion(self):
-        if not self.conn_established:
-            return
-        self.envio_paquetes_seguro(_flags='F')
-        while self.last_pkt_rcvd[TCP].flags != 'FA':
-            self.listen()
-
-        self.envio_paquetes_seguro(_flags='A')
+    pkt_capturado = sniff(
+        iface=interface, prn=info_packet, count=1, timeout=timeout_, filter=filter_str,)
+    return pkt_capturado
