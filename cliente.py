6495584db9b8389588a58071eed95825c3ff1eda
NicoPalazon <nicolaspalazon01@gmail.com>
2024-10-17 18:44:13 -0300

    a

--- cliente.py
+++ cliente.py
@@ -7,49 +7,52 @@ dest_port = 8000
 src_port = 5000
 seq_ = 500
 ack_ = None
-
 tcp_pkt = None
-threads = []
+i = 0
 
-while not tcp_pkt or (TCP in tcp_pkt[0] and tcp_pkt[0][TCP].ack < seq_ + 1):
+while i < 50:
+    seq_ = 500
+    ack_ = None
     tcp_pkt = None
-    f.envio_paquetes_inseguro(enviar_pkt(seq_, 0, "S", dest_ip, source_ip, dest_port, src_port))
-    tcp_pkt = escuchar(3, src_port)
-
-rcv = tcp_pkt[0][TCP]
-ack_ = rcv.seq
-seq_ = rcv.ack
-tcp_pkt = None
-threads.clear()
-
-while True:
-    tcp_pkt = escuchar(3, src_port)
-
-    if tcp_pkt and TCP in tcp_pkt[0] and tcp_pkt[0][TCP].ack != seq_ + 1:
-        f.envio_paquetes_inseguro(enviar_pkt(seq_, ack_ + 1, "A", dest_ip, source_ip, dest_port, src_port))
+    while not tcp_pkt or (TCP in tcp_pkt[0] and tcp_pkt[0][TCP].ack < seq_ + 1):
         tcp_pkt = None
+        f.envio_paquetes_inseguro(enviar_pkt(seq_, 0, "S", dest_ip, source_ip, dest_port, src_port))
+        tcp_pkt = escuchar(3, src_port)
 
-    elif tcp_pkt and TCP in tcp_pkt[0] and tcp_pkt[0][TCP].ack == seq_ + 1:
-        break
+    rcv = tcp_pkt[0][TCP]
+    ack_ = rcv.seq
+    seq_ = rcv.ack
+    tcp_pkt = None
 
-rcv = tcp_pkt[0][TCP]
-ack_ = rcv.seq
-seq_ = rcv.ack
-tcp_pkt = None
+    while True:
+        tcp_pkt = escuchar(3, src_port)
 
-timer_ = 60
-start_time = time.time()
+        if tcp_pkt and TCP in tcp_pkt[0] and tcp_pkt[0][TCP].ack != seq_ + 1:
+            f.envio_paquetes_inseguro(enviar_pkt(seq_, ack_ + 1, "A", dest_ip, source_ip, dest_port, src_port))
+            tcp_pkt = None
 
-while time.time() - start_time < timer_:
-    
-    if tcp_pkt and TCP in tcp_pkt[0] and tcp_pkt[0][TCP].ack != seq_ + 1:
-        f.envio_paquetes_inseguro(enviar_pkt(seq_, ack_ + 1, "FA", dest_ip, source_ip, dest_port, src_port))
+        elif tcp_pkt and TCP in tcp_pkt[0] and tcp_pkt[0][TCP].ack == seq_ + 1:
+            break
 
-    elif tcp_pkt and TCP in tcp_pkt[0] and tcp_pkt[0][TCP].ack == seq_ + 1:
-        break
+    rcv = tcp_pkt[0][TCP]
+    ack_ = rcv.seq
+    seq_ = rcv.ack
+    tcp_pkt = None
 
-    tcp_pkt = escuchar(3, src_port)
+    timer_ = 60
+    start_time = time.time()
 
+    while time.time() - start_time < timer_:
+        
+        if tcp_pkt and TCP in tcp_pkt[0] and tcp_pkt[0][TCP].ack != seq_ + 1:
+            f.envio_paquetes_inseguro(enviar_pkt(seq_, ack_ + 1, "FA", dest_ip, source_ip, dest_port, src_port))
+
+        elif tcp_pkt and TCP in tcp_pkt[0] and tcp_pkt[0][TCP].ack == seq_ + 1:
+            break
+
+        tcp_pkt = escuchar(3, src_port)
+
+    i += 1
 
 
 
--- servidor.py
+++ servidor.py
@@ -6,46 +6,51 @@ dest_port = 5000
 src_port = 8000
 seq_ = 5
 ack_ = None
+i = 0
 
-tcp_pkt = escuchar(60, src_port)
-
-
-if tcp_pkt:
-    ack_ = tcp_pkt[0][TCP].seq
-    tcp_pkt = None
-    
-    while not tcp_pkt or (TCP in tcp_pkt[0] and tcp_pkt[0][TCP].ack < seq_ + 1):
-        f.envio_paquetes_inseguro(enviar_pkt(seq_, ack_ + 1, "SA", dest_ip, source_ip, dest_port, src_port))
-        tcp_pkt = escuchar(3, src_port)
-
-            
-rcv = tcp_pkt[0][TCP]
-ack_ = rcv.seq
-seq_ = rcv.ack
-tcp_pkt = None
+while i < 50:
+    seq_ = 5
+    ack_ = None
+    tcp_pkt = escuchar(60, src_port)
 
 
-timer_ = 20
-time.sleep(timer_)
-
-while not tcp_pkt or (TCP in tcp_pkt[0] and tcp_pkt[0][TCP].ack < seq_ + 1):
-        f.envio_paquetes_inseguro(enviar_pkt(seq_, ack_ + 1, "F", dest_ip, source_ip, dest_port, src_port))
-        tcp_pkt = escuchar(3, src_port)
-        
-
-timer_ = 20
-start_time = time.time()
-
-
-rcv = tcp_pkt[0][TCP]
-ack_ = rcv.seq
-seq_ = rcv.ack
-tcp_pkt = None
-f.envio_paquetes_inseguro(enviar_pkt(seq_, ack_ + 1, "A", dest_ip, source_ip, dest_port, src_port))
-
-while time.time() - start_time < timer_:
-    if tcp_pkt and TCP in tcp_pkt[0] and tcp_pkt[0][TCP].ack == seq_ + 1:
-        f.envio_paquetes_inseguro(enviar_pkt(seq_, ack_ + 1, "A", dest_ip, source_ip, dest_port, src_port))
+    if tcp_pkt:
+        ack_ = tcp_pkt[0][TCP].seq
         tcp_pkt = None
-    tcp_pkt = escuchar(3, src_port)
         
+        while not tcp_pkt or (TCP in tcp_pkt[0] and tcp_pkt[0][TCP].ack < seq_ + 1):
+            f.envio_paquetes_inseguro(enviar_pkt(seq_, ack_ + 1, "SA", dest_ip, source_ip, dest_port, src_port))
+            tcp_pkt = escuchar(3, src_port)
+
+                
+    rcv = tcp_pkt[0][TCP]
+    ack_ = rcv.seq
+    seq_ = rcv.ack
+    tcp_pkt = None
+
+
+    timer_ = 20
+    time.sleep(timer_)
+
+    while not tcp_pkt or (TCP in tcp_pkt[0] and tcp_pkt[0][TCP].ack < seq_ + 1):
+            f.envio_paquetes_inseguro(enviar_pkt(seq_, ack_ + 1, "F", dest_ip, source_ip, dest_port, src_port))
+            tcp_pkt = escuchar(3, src_port)
+            
+
+    timer_ = 20
+    start_time = time.time()
+
+
+    rcv = tcp_pkt[0][TCP]
+    ack_ = rcv.seq
+    seq_ = rcv.ack
+    tcp_pkt = None
+    f.envio_paquetes_inseguro(enviar_pkt(seq_, ack_ + 1, "A", dest_ip, source_ip, dest_port, src_port))
+
+    while time.time() - start_time < timer_:
+        if tcp_pkt and TCP in tcp_pkt[0] and tcp_pkt[0][TCP].ack == seq_ + 1:
+            f.envio_paquetes_inseguro(enviar_pkt(seq_, ack_ + 1, "A", dest_ip, source_ip, dest_port, src_port))
+            tcp_pkt = None
+        tcp_pkt = escuchar(3, src_port)
+            
+    i += 1
