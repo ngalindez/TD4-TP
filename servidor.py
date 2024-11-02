2828ebbe837213c2d9372b2e5d0e69b452ba51f9
Martín Kaplan <martinarielkaplan@gmail.com>
2024-11-02 14:25:11 -0300

    FINAL FINAL FINAL

--- servidor.py
+++ servidor.py
@@ -79,7 +79,7 @@ def conexion_servidor(source_ip,src_port,interface):
         tcp_pkt = listen(3, src_port,interface)
         # Si en esos 20 segundo le me vuelve a llegar un paquete y es el de FA del cliente (con Checksum correcto),
         # le vuelvo a enviar el A.
-        if tcp_pkt and TCP in tcp_pkt[0] and tcp_pkt[0][TCP].ack >= seq_ and verify_checksum(tcp_pkt[0]):
+        if correcto(tcp_pkt,seq_-1,ack_,dest_port,flags="FA"):
             packet = build_pkt(seq_, ack_ + 1, "A", dest_ip,
                                source_ip, dest_port, src_port)
             f.envio_paquetes_inseguro(packet)
